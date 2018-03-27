6cecace2dad905e43c141cdb77072c5a175f7859
moottier <andre.mottier@gmail.com>
2018-03-27 07:24:10 -0700

    trying to workaround PA using django 1.11

--- halies_site/urls.py
+++ halies_site/urls.py
@@ -14,8 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+#from django.urls import path #WILL NOT WORK WITH DJANGO 1.11 running on PA
+from django.conf.urls import url
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url('admin/', admin.site.urls),
 ]
